f792d1d44506f273760b678121345df78bc2b952
Velma Kimani <stanvelma16@gmail.com>
2023-10-02 17:58:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(booksData => booksData.forEach(book => renderBooks(book) 
+  ))
+  console.log('before fetch returns')
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  //books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = books.name;
     main.appendChild(h2);
-  });
+ // });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function initialize(){
+    fetchBooks()
+    console.log('after get all books')
+}
+initialize()
